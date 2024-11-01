07251a8eaa8f2633ca4c17cd3748e83eea64840f
YaexMiko <aloksinghmains756@gmail.com>
2024-11-01 23:13:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002280990808"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "7512463676"))
 
 #Port
-PORT = os.environ.get("PORT", "8380")
+PORT = os.environ.get("PORT", "8180")
 
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Premium:aloksingh@cluster0.4ykpo.mongodb.net/?retryWrites=true&w=majority")
