1631c9248bec5eb867db2ca6dd1229a6881efb00
YaexMiko <aloksinghmains756@gmail.com>
2024-11-01 23:12:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "20071888"))
 API_HASH = os.environ.get("API_HASH", "1c4cb9d94b23282abd9ae2a87a521b53")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001919223553"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002280990808"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "7512463676"))
@@ -29,13 +29,13 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7512463676"))
 PORT = os.environ.get("PORT", "8380")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Free:aloksingh@cluster0.tphfi.mongodb.net/?retryWrites=true&w=majority")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Premium:aloksingh@cluster0.4ykpo.mongodb.net/?retryWrites=true&w=majority")
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DB_URI)
 DB_NAME = os.environ.get("DATABASE_NAME", "Free")
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002445549316"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002280973372"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002339478207"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002422843872"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
